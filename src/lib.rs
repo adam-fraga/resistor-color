6522fb900349840341f7985207b3ab830496dcdb
adam-tekoway <adam.fraga@tekoway.com>
2022-05-22 18:46:58 +0200

    done

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,38 @@
-#[derive(Debug, PartialEq)]
+// The goal of this exercise is to create a way:
+// - to look up the numerical value associated with a particular color band
+// - to convert the numerical value into a string representing color
+// - to list the different band colors
+use enum_iterator::IntoEnumIterator;
+use int_enum::IntEnum;
+
+#[repr(usize)]
+#[derive(IntoEnumIterator, Copy, Clone, Debug, Eq, PartialEq, IntEnum, Ord, PartialOrd)]
 pub enum ResistorColor {
-    Black,
-    Blue,
-    Brown,
-    Green,
-    Grey,
-    Orange,
-    Red,
-    Violet,
-    White,
-    Yellow,
+    Black = 0,
+    Blue = 6,
+    Brown = 1,
+    Green = 5,
+    Grey = 8,
+    Orange = 3,
+    Red = 2,
+    Violet = 7,
+    White = 9,
+    Yellow = 4,
 }
 
 pub fn color_to_value(_color: ResistorColor) -> usize {
-    unimplemented!("convert a color into a numerical representation")
+    _color.int_value()
 }
 
 pub fn value_to_color_string(value: usize) -> String {
-    unimplemented!(
-        "convert the value {} into a string representation of color",
-        value
-    )
+    match ResistorColor::from_int(value) {
+        Ok(color) => format!("{:?}", color),
+        _ => String::from("value out of range"),
+    }
 }
 
 pub fn colors() -> Vec<ResistorColor> {
-    unimplemented!("return a list of all the colors ordered by resistance")
+    let mut colors: Vec<ResistorColor> = ResistorColor::into_enum_iter().collect::<Vec<ResistorColor>>();
+    colors.sort();
+    colors
 }
